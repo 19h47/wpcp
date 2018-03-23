2fdc3cc2b7121cbe8b04a04eb54a024fac096247
19h47 <levronjeremy@19h47.fr>
2018-03-23 09:54:58 +0100

    [WIP] Trying to fix media_handle_upload

--- public/class-wpcp-public.php
+++ public/class-wpcp-public.php
@@ -81,7 +81,7 @@ class WPCP_Public {
 			// Event title
 			'post_type'     => 'event',
 			'post_status'   => 'draft',
-			'post_title'    => $posted_data[event_title],
+			'post_title'    => $posted_data['event_title'],
 			'post_status'   => 'draft',
 		);
 
@@ -93,7 +93,7 @@ class WPCP_Public {
 		);
 
 		foreach ( $cities as $city ) {
-			if ( $city->name === $posted_data[place_participating_city] ) {
+			if ( $city->name === $posted_data['place_participating_city'] ) {
 				$your_post['post_author'] = get_field( 'user', 'city_' . $city->term_id )['ID'];
 
 				break;
@@ -110,20 +110,20 @@ class WPCP_Public {
 		// WPGM
 		$address = [];
 
-		if ( $posted_data[place_address] ) {
-			array_push( $address, $posted_data[place_address] );
+		if ( $posted_data['place_address'] ) {
+			array_push( $address, $posted_data['place_address'] );
 		}
 
-		if ( $posted_data[place_city] ) {
-			array_push( $address, $posted_data[place_city] );
+		if ( $posted_data['place_city'] ) {
+			array_push( $address, $posted_data['place_city'] );
 		}
 
-		if ( $posted_data[place_address_supplement] ) {
-			array_push( $address, $posted_data[place_address_supplement] );
+		if ( $posted_data['place_address_supplement'] ) {
+			array_push( $address, $posted_data['place_address_supplement'] );
 		}
 
-		if ( $posted_data[place_postal_code] ) {
-			array_push( $address, $posted_data[place_postal_code] );
+		if ( $posted_data['place_postal_code'] ) {
+			array_push( $address, $posted_data['place_postal_code'] );
 		}
 
 		update_post_meta( $post_id, '_wpgm_details', array( 'address' => join( ', ', $address ) ) );
@@ -136,38 +136,38 @@ class WPCP_Public {
 		/////////////////////
 
 		// Particular
-		if ( $posted_data[particular] ) {
-			update_field( 'field_5a6a583c34e75', $posted_data[particular], $post_id );
+		if ( $posted_data['particular'] ) {
+			update_field( 'field_5a6a583c34e75', $posted_data['particular'], $post_id );
 		}
 
 		// Association name
-		if ( $posted_data[association_name] ) {
-			 update_field( 'field_5a6a574df7240', $posted_data[association_name], $post_id );
+		if ( $posted_data['association_name'] ) {
+			 update_field( 'field_5a6a574df7240', $posted_data['association_name'], $post_id );
 		}
 
 		// Association email
-		if ( $posted_data[association_email] ) {
-			update_field( 'field_5a6a566a52e71', $posted_data[association_email], $post_id );
+		if ( $posted_data['association_email'] ) {
+			update_field( 'field_5a6a566a52e71', $posted_data['association_email'], $post_id );
 		}
 
 		// Association phone
-		if ( $posted_data[association_phone] ) {
-			 update_field( 'field_5a6a5738f723f', $posted_data[association_phone], $post_id );
+		if ( $posted_data['association_phone'] ) {
+			 update_field( 'field_5a6a5738f723f', $posted_data['association_phone'], $post_id );
 		}
 
 		// Place name
-		if ( $posted_data[place_name] ) {
-			update_field( 'field_5a75ed0e3532a', $posted_data[place_name], $post_id );
+		if ( $posted_data['place_name'] ) {
+			update_field( 'field_5a75ed0e3532a', $posted_data['place_name'], $post_id );
 		}
 
 		// Place email
-		if ( $posted_data[place_email] ) {
-			update_field( 'field_5a75ed9c3532b', $posted_data[place_email], $post_id );
+		if ( $posted_data['place_email'] ) {
+			update_field( 'field_5a75ed9c3532b', $posted_data['place_email'], $post_id );
 		}
 
 		// Place phone
-		if ( $posted_data[place_phone] ) {
-			update_field( 'field_5a75edbe3532c', $posted_data[place_phone], $post_id );
+		if ( $posted_data['place_phone'] ) {
+			update_field( 'field_5a75edbe3532c', $posted_data['place_phone'], $post_id );
 		}
 
 
@@ -176,28 +176,28 @@ class WPCP_Public {
 		/////////////////////////////
 
 		// your_civility
-		if ( $posted_data[your_civility] ) {
-			update_field( 'field_5a6ba39fd700e', $posted_data[your_civility], $post_id );
+		if ( $posted_data['your_civility'] ) {
+			update_field( 'field_5a6ba39fd700e', $posted_data['your_civility'], $post_id );
 		}
 
 		// your_name
-		if ( $posted_data[your_name] ) {
-			update_field( 'field_5a6ba492f464a', $posted_data[your_name], $post_id );
+		if ( $posted_data['your_name'] ) {
+			update_field( 'field_5a6ba492f464a', $posted_data['your_name'], $post_id );
 		}
 
 		// your_first_name
-		if ( $posted_data[your_first_name] ) {
-			update_field( 'field_5a6ba488f4649', $posted_data[your_first_name], $post_id );
+		if ( $posted_data['your_first_name'] ) {
+			update_field( 'field_5a6ba488f4649', $posted_data['your_first_name'], $post_id );
 		}
 
 		// your_email
-		if ( $posted_data[your_email] ) {
-			update_field( 'field_5a6ba50af464b', $posted_data[your_email], $post_id );
+		if ( $posted_data['your_email'] ) {
+			update_field( 'field_5a6ba50af464b', $posted_data['your_email'], $post_id );
 		}
 
 		// your_phone
-		if ( $posted_data[your_phone] ) {
-			update_field( 'field_5a6ba538f464c', $posted_data[your_phone], $post_id );
+		if ( $posted_data['your_phone'] ) {
+			update_field( 'field_5a6ba538f464c', $posted_data['your_phone'], $post_id );
 		}
 
 
@@ -206,43 +206,43 @@ class WPCP_Public {
 		/////////////////////////
 
 		// place_type
-		if ( $posted_data[place_type] ) {
-			update_field( 'field_5a6b9efc1ca76', $posted_data[place_type], $post_id );
+		if ( $posted_data['place_type'] ) {
+			update_field( 'field_5a6b9efc1ca76', $posted_data['place_type'], $post_id );
 		}
 
 		// place_public_name
-		if ( $posted_data[place_public_name] ) {
-			update_field( 'field_5a5fc10ae7b4d', $posted_data[place_public_name], $post_id );
+		if ( $posted_data['place_public_name'] ) {
+			update_field( 'field_5a5fc10ae7b4d', $posted_data['place_public_name'], $post_id );
 		}
 
 		// place_participating_city
-		if ( $posted_data[place_participating_city] ) {
+		if ( $posted_data['place_participating_city'] ) {
 			wp_set_object_terms(
 				$post_id,
-				$posted_data[place_participating_city],
+				$posted_data['place_participating_city'],
 				'city',
 				false
 			);
 		}
 
 		// place_address
-		if ( $posted_data[place_address] ) {
-			update_field( 'field_5a6ba027c718b', $posted_data[place_address], $post_id );
+		if ( $posted_data['place_address'] ) {
+			update_field( 'field_5a6ba027c718b', $posted_data['place_address'], $post_id );
 		}
 
 		// place_city
-		if ( $posted_data[place_city] ) {
-			update_field( 'field_5a6ba049c718c', $posted_data[place_city], $post_id );
+		if ( $posted_data['place_city'] ) {
+			update_field( 'field_5a6ba049c718c', $posted_data['place_city'], $post_id );
 		}
 
 		// place_address_supplement
-		if ( $posted_data[place_address_supplement] ) {
-			update_field( 'field_5a6ba072c718d', $posted_data[place_address_supplement], $post_id );
+		if ( $posted_data['place_address_supplement'] ) {
+			update_field( 'field_5a6ba072c718d', $posted_data['place_address_supplement'], $post_id );
 		}
 
 		// place_postal_code
-		if ( $posted_data[place_postal_code] ) {
-			update_field( 'field_5a6ba0a3c718e', $posted_data[place_postal_code], $post_id );
+		if ( $posted_data['place_postal_code'] ) {
+			update_field( 'field_5a6ba0a3c718e', $posted_data['place_postal_code'], $post_id );
 		}
 
 
@@ -251,10 +251,10 @@ class WPCP_Public {
 		////////////////////////////////
 
 		// event_category
-		if ( $posted_data[event_category] ) {
+		if ( $posted_data['event_category'] ) {
 			wp_set_object_terms(
 				$post_id,
-				$posted_data[event_category],
+				$posted_data['event_category'],
 				'event_category',
 				false
 			);
@@ -262,151 +262,172 @@ class WPCP_Public {
 
 
 		// Friday
-		$fridays = explode( ',', $posted_data[friday_repeater] );
+		$fridays = explode( ',', $posted_data['friday_repeater'] );
 		$fridays = array_splice( $fridays , 1, count( $fridays ) );
 		$fridays_value = [];
 
-		for ( $i = 0; $i <= ( count( $fridays ) / 3 ) - 1; $i++ ) {
-			$index = "-{$i}";
+		if ( ! empty( $posted['event_date_friday_hour'] ) && ! empty( $posted['event_date_friday_duration'] ) && ! empty( $posted['event_date_friday_end'] ) ) {
 
-			if ( $index === '-0' ) {
-				$index = '';
+			for ( $i = 0; $i <= ( count( $fridays ) / 3 ) - 1; $i++ ) {
+				$index = "-{$i}";
+
+				if ( $index === '-0' ) {
+					$index = '';
+				}
+
+				$friday_value = array(
+					'hour'      => $posted_data['event_date_friday_hour' . $index],
+					'duration'  => $posted_data['event_date_friday_duration' . $index],
+					'end'       => $posted_data['event_date_friday_end' . $index],
+				);
+
+				if ( $posted_data['event_places_number'] ) {
+					$friday_value['event_places_number'] = $posted_data['event_places_number'];
+				}
+
+				array_push( $fridays_value, $friday_value );
 			}
-
-			$friday_value = array(
-				'hour'      			=> $posted_data['event_date_friday_hour' . $index],
-				'duration'  			=> $posted_data['event_date_friday_duration' . $index],
-				'end'       			=> $posted_data['event_date_friday_end' . $index],
-			);
-
-			if ( $posted_data[event_places_number] ) {
-				$friday_value['event_places_number'] = $posted_data['event_places_number' . $index];
-			}
-
-			array_push( $fridays_value, $friday_value );
+			update_field( 'field_5aaa221e302f8', $fridays_value, $post_id );
 		}
-		update_field( 'field_5aaa221e302f8', $fridays_value, $post_id );
 
 
 		// Saturday
-		$saturdays = explode( ',', $posted_data[saturday_repeater] );
+		$saturdays = explode( ',', $posted_data['saturday_repeater'] );
 		$saturdays = array_splice( $saturdays , 1, count( $saturdays ) );
 		$saturdays_value = [];
 
-		for ( $i = 0; $i <= ( count( $saturdays ) / 3 ) - 1; $i++ ) {
-			$index = "-{$i}";
+		if ( ! empty( $posted['event_date_saturday_hour'] ) && ! empty( $posted['event_date_saturday_duration'] ) && ! empty( $posted['event_date_saturday_end'] ) ) {
 
-			if ( $index === '-0' ) {
-				$index = '';
+			for ( $i = 0; $i <= ( count( $saturdays ) / 3 ) - 1; $i++ ) {
+				$index = "-{$i}";
+
+				if ( $index === '-0' ) {
+					$index = '';
+				}
+
+				$saturday_value = array(
+					'hour'      => $posted_data['event_date_saturday_hour' . $index],
+					'duration'  => $posted_data['event_date_saturday_duration' . $index],
+					'end'       => $posted_data['event_date_saturday_end' . $index],
+				);
+
+				if ( $posted_data['event_places_number'] ) {
+					$saturday_value['event_places_number'] = $posted_data['event_places_number'];
+				}
+
+				array_push( $saturdays_value, $saturday_value );
 			}
-
-			$saturday_value = array(
-				'hour'      			=> $posted_data['event_date_saturday_hour' . $index],
-				'duration'  			=> $posted_data['event_date_saturday_duration' . $index],
-				'end'       			=> $posted_data['event_date_saturday_end' . $index],
-			);
-
-			if ( $posted_data[event_places_number] ) {
-				$sunday_value['event_places_number'] = $posted_data['event_places_number' . $index];
-			}
-
-			array_push( $saturdays_value, $saturday_value );
+			update_field( 'field_5a6c8c24e73e4', $saturdays_value, $post_id );
 		}
-		update_field( 'field_5a6c8c24e73e4', $saturdays_value, $post_id );
+
 
 
 		// Sunday
-		$sundays = explode( ',', $posted_data[sunday_repeater] );
+		$sundays = explode( ',', $posted_data['sunday_repeater'] );
 		$sundays = array_splice( $sundays , 1, count( $sundays ) );
 		$sundays_value = [];
 
-		for ( $i = 0; $i <= ( count( $sundays ) / 3 ) - 1; $i++ ) {
-			$index = "-{$i}";
+		if ( ! empty( $posted['event_daundayrday_hour'] ) && ! empty( $posted['event_date_sunday_duration'] ) && ! empty( $posted['event_dundayurday_end'] ) ) {
 
-			if ( $index === '-0' ) {
-				$index = '';
+			for ( $i = 0; $i <= ( count( $sundays ) / 3 ) - 1; $i++ ) {
+				$index = "-{$i}";
+
+				if ( $index === '-0' ) {
+					$index = '';
+				}
+
+				$sunday_value = array(
+					'hour'      => $posted_data['event_date_sunday_hour' . $index],
+					'duration'  => $posted_data['event_date_sunday_duration' . $index],
+					'end'       => $posted_data['event_date_sunday_end' . $index],
+				);
+
+				if ( $posted_data['event_places_number'] ) {
+					$sunday_value['event_places_number'] = $posted_data['event_places_number'];
+				}
+
+				array_push( $sundays_value, $sunday_value );
 			}
-
-			$sunday_value = array(
-				'hour'      			=> $posted_data['event_date_sunday_hour' . $index],
-				'duration'  			=> $posted_data['event_date_sunday_duration' . $index],
-				'end'       			=> $posted_data['event_date_sunday_end' . $index],
-			);
-
-			if ( $posted_data[event_places_number] ) {
-				$sunday_value['event_places_number'] = $posted_data['event_places_number' . $index];
-			}
-
-			array_push( $sundays_value, $sunday_value );
+			update_field( 'field_5a6c8cd3e6712', $sundays_value, $post_id );
 		}
-		update_field( 'field_5a6c8cd3e6712', $sundays_value, $post_id );
 
 
 		// event_prices
-		if ( $posted_data[event_prices] ) {
-			update_field( 'field_5a6c8d41e6717', $posted_data[event_prices], $post_id );
+		if ( $posted_data['event_prices'] ) {
+			update_field( 'field_5a6c8d41e6717', $posted_data['event_prices'], $post_id );
 		}
 
 		// event_places_number
-		if ( $posted_data[event_places_number] ) {
-			update_field( 'field_5a70ec191d80f', $posted_data[event_places_number], $post_id );
+		if ( $posted_data['event_places_number'] ) {
+			update_field( 'field_5a70ec191d80f', $posted_data['event_places_number'], $post_id );
 		}
 
 		// event_minimum_ages
-		if ( $posted_data[event_minimum_ages] ) {
-			update_field( 'field_5a6c8d5de6718', $posted_data[event_minimum_ages], $post_id );
+		if ( $posted_data['event_minimum_ages'] ) {
+			update_field( 'field_5a6c8d5de6718', $posted_data['event_minimum_ages'], $post_id );
 		}
 
 		// event_description
-		if ( $posted_data[event_description] ) {
-			update_field( 'field_5a633a9d94992', $posted_data[event_description], $post_id );
+		if ( $posted_data['event_description'] ) {
+			update_field( 'field_5a633a9d94992', $posted_data['event_description'], $post_id );
 		}
 
 		////////////////////
 		// Ajout de média //
 		////////////////////
 
-		require_once( ABSPATH . 'wp-admin/includes/image.php' );
 		require_once( ABSPATH . 'wp-admin/includes/file.php' );
+		require_once( ABSPATH . 'wp-admin/includes/image.php' );
 		require_once( ABSPATH . 'wp-admin/includes/media.php' );
 
+		var_dump( $_FILES );
 
 		// image
-		$event_attachments = ['event_image_1', 'event_image_2', 'event_image_3'];
+		$event_attachments = ['event-image-1', 'event-image-2', 'event-image-3'];
 		$event_attachments_id = [];
 
-		for ( $i = 0; $i <= count( $event_attachments ); $i++ ) {
+		media_handle_upload('img', 0 );
 
-			$event_attachment_id = media_handle_upload( (string) $event_attachments[$i], 0 );
+		foreach ( $event_attachments as $event_attachment ) {
 
-			if ( $event_attachment_id ) {
-				array_push( $event_attachments_id, $event_attachment_id );
-			}
+			$event_attachment_id = media_handle_upload( $event_attachment, 0 );
+
+			var_dump( $event_attachment );
+
+			var_dump( $event_attachment_id );
+
+			if ( is_wp_error( $event_attachment_id ) ) continue;
+
+			array_push( $event_attachments_id, $event_attachment_id );
 		}
 		update_field( 'field_5a6dd43daa1dc', $event_attachments_id, $post_id );
 
 
 		// partners
-		$partners = explode( ',', $posted_data[partners_repeater] );
+		// Get partners repeater
+		$partners = explode( ',', $posted_data['partners_repeater'] );
+		// Remove the first entry (Useless for media upload)
 		$partners = array_splice( $partners , 1, count( $partners ) );
 		$partners_attachment_id = [];
 
-		for ( $i = 0; $i <= count( $partners ); $i++ ) {
-			$partner_attachment_id = media_handle_upload( $partners[$i], 0 );
+		foreach ( $partners as $partner ) {
 
-			if ( $partner_attachment_id ) {
-				array_push( $partners_attachment_id, $partner_attachment_id );
-			}
+			$partner_attachment_id = media_handle_upload( $partner, 0 );
+
+			// There was an error uploading the image.
+			if ( is_wp_error( $partner_attachment_id ) ) continue;
+
+			array_push( $partners_attachment_id, $partner_attachment_id );
 		}
 		update_field( 'field_5a6dcdd1766c1', $partners_attachment_id, $post_id );
 
 
 		// youtube
-		if ( $posted_data[youtube] ) {
-			update_field( 'field_5a6dcd81a44b5', $posted_data[youtube], $post_id );
+		if ( $posted_data['youtube'] ) {
+			update_field( 'field_5a6dcd81a44b5', $posted_data['youtube'], $post_id );
 		}
 
-		// wp_die();
+		// wp_die( $posted_data );
 
 		return $posted_data;
 	}
